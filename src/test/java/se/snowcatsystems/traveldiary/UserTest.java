b9a68f042be7e3493759cc024f26c9fdc672b6c7
Björn Kristensson Alfsson <bjorn.alfsson@gmail.com>
2020-03-27 12:44:39 +0100

    Removed unused import

--- src/test/java/se/snowcatsystems/traveldiary/UserTest.java
+++ src/test/java/se/snowcatsystems/traveldiary/UserTest.java
@@ -15,7 +15,6 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.MySQLContainer;
-import se.snowcatsystems.traveldiary.MySqlContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
