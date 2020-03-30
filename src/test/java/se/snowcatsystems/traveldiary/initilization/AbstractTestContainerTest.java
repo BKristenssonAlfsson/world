814fe665bfd1d924c2395deffb63d060d9aaaab8
Björn Kristensson Alfsson <bjorn.alfsson@gmail.com>
2020-03-30 08:49:18 +0200

    Removed typos

--- src/test/java/se/snowcatsystems/traveldiary/initilization/AbstractTestContainerTest.java
+++ src/test/java/se/snowcatsystems/traveldiary/initilization/AbstractTestContainerTest.java
@@ -1,5 +1,5 @@
 package se.snowcatsystems.traveldiary.initilization;
-?
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +18,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @AutoConfigureMockMvc
 @ContextConfiguration(initializers = {AbstractTestContainerTest.Initializer.class})
 @Testcontainers
-public abstract class AbstractTestContainerTest {
+public abstract class AbstractTestContainerTest  {
 
     @Autowired
     public MockMvc mockMvc;
