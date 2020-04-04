040513caad5b28b7564487392658c6314f81e16a
Björn Kristensson Alfsson <bjorn.alfsson@gmail.com>
2020-04-04 08:43:09 +0200

    Added endpoint for updating language

--- src/main/java/se/snowcatsystems/traveldiary/security/SecurityConfiguration.java
+++ src/main/java/se/snowcatsystems/traveldiary/security/SecurityConfiguration.java
@@ -51,6 +51,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/country").access("hasRole('USER') or hasRole('ADMIN')")
                 .antMatchers(HttpMethod.GET, "/language").access("hasRole('USER') or hasRole('ADMIN')")
                 .antMatchers(HttpMethod.POST, "/language").access("hasRole('USER') or hasRole('ADMIN')")
+                .antMatchers(HttpMethod.PATCH, "/language").access("hasRole('USER') or hasRole('ADMIN')")
                 .anyRequest().denyAll();
     }
 
