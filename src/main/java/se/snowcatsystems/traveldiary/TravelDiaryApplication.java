7b3586b716f44260a12f04e72f9488dd0ddd1bab
Björn Kristensson Alfsson <bjorn.alfsson@gmail.com>
2020-04-17 13:11:02 +0200

    Added Todos

--- src/main/java/se/snowcatsystems/traveldiary/TravelDiaryApplication.java
+++ src/main/java/se/snowcatsystems/traveldiary/TravelDiaryApplication.java
@@ -8,6 +8,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class TravelDiaryApplication {
 
+    /*
+    TODO: Add Exception handling
+    TODO: Add User account details (blog, saved travels etc)
+    TODO: When searching for a country, list all cities connected to it.
+     */
     public static void main(String[] args) {
         SpringApplication.run(TravelDiaryApplication.class, args);
     }
